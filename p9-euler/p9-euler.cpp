c14be7adaf86a340c2de182fb9bb5ab787baf436
Christian Häge <christian.haege@gmx.net>
2019-03-17 12:44:10 +0100

    Added visual studio project

---
+++ p9-euler/p9-euler.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+
+int main(int argc, char **argv)
+{
+	unsigned long long p9_answ = 0;
+
+
+	std::cout << "The answer to the 9th problem of ProjectEuler.Net is " << p9_answ << ".\n";
+
+
+	return 0;
+}
