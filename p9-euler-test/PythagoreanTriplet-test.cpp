4c4efd0fd521bf9776b73b9f4ac7079e214ec71e
chaege_Thinkpad <c.haege@ipc.uni-stuttgart.de>
2019-03-20 18:07:40 +0100

    Added sqrt function for integers

--- p9-euler-test/PythagoreanTriplet-test.cpp
+++ p9-euler-test/PythagoreanTriplet-test.cpp
@@ -17,10 +17,19 @@ TEST(PythagoreanTripletIsAClass, Exists) {
 }
 
 
-TEST_F(PythagoreanTripletTestClass, FindSimplePythagoreanTriple)
+TEST(PythagoreanTripletSqrtTest, SQRTofSimpleInteger) {	
+	ASSERT_EQ(PythagoreanTriplet::sqrt(1), 1);
+	ASSERT_EQ(PythagoreanTriplet::sqrt(25), 5);
+	ASSERT_EQ(PythagoreanTriplet::sqrt(81), 9);
+	ASSERT_EQ(PythagoreanTriplet::sqrt(555), 308025);
+	ASSERT_EQ(PythagoreanTriplet::sqrt(80), 0);
+}
+
+
+TEST_F(PythagoreanTripletTestClass, FindSimplePythagoreanTripleEqualToSum)
 {
 	std::vector<unsigned int> triple = { 3,4,5 };
-	ASSERT_EQ(pythago.find_pythagorean_triple(12), triple);
+	ASSERT_EQ(pythago.find_pythagorean_triple_of_sum(12), triple);
 }
 
 
--- p9-euler/PythagoreanTriplet.h
+++ p9-euler/PythagoreanTriplet.h
@@ -2,15 +2,17 @@
 
 
 #include <vector>
+#include <cmath>
 
 
 class PythagoreanTriplet
 {
 public:
-	std::vector<unsigned int> find_pythagorean_triple(unsigned int sum_of_abc) const
+	std::vector<unsigned int> find_pythagorean_triple_of_sum(unsigned int sum_of_abc) const
 	{
 		std::vector<unsigned int> abc;
 		unsigned int a = 0, b = 0, c = 0;
+
 
 
 
@@ -19,6 +21,35 @@ public:
 		abc.push_back(c);
 		return abc;
 	}
+
+
+	static unsigned int sqrt(unsigned int val)
+	{
+		unsigned int sqrt = (unsigned int)(std::sqrt(double(val)));
+
+		if ((sqrt*sqrt) == val) return sqrt;
+
+		return 0;
+	}
+
+private:
+	unsigned int find_second_value(unsigned int first_val, unsigned int sum_of_abc) const
+	{
+		unsigned int b = first_val, c = first_val, sum = 0;
+		
+		while (sum <= sum_of_abc)
+		{
+			
+		}	
+		
+
+
+		return b;
+	}
+
+
+	
+
 
 };
 
